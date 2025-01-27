ef3f31d39f572d199e615830029d5f098cc85d69
V1GENIUS <Vivekrathore9111@gmail.com>
2025-01-27 21:33:55 +0530

    finelsssgs

--- src/Pages/GovtJobDetails.jsx
+++ src/Pages/GovtJobDetails.jsx
@@ -148,7 +148,7 @@ function GovtJobDetails() {
   *Selection Process* : ${jobDetails?.selectionProcess || "N/A"}
   *Job Location* : ${jobDetails.jobLocation?.location || "N/A"}
   *Qualification* : ${jobDetails?.Qualification?.eligibility || "N/A"}
-  *Apply Online* : ${jobDetails.applyLink || "N/A"}
+
   *Job Details Link* : ${jobLink}
     `;
   };
