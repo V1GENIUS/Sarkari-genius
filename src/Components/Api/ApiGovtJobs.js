9432d4f184ea02af842551854a01ae5a00969ed2
V1GENIUS <Vivekrathore9111@gmail.com>
2025-01-28 13:54:36 +0530

    cache committ

--- src/Components/Api/ApiGovtJobs.js
+++ src/Components/Api/ApiGovtJobs.js
@@ -1,7 +1,7 @@
 
 
-// const BASE_URL = "https://sarkari-genius.onrender.com";
-const BASE_URL = "http://localhost:7000";
+const BASE_URL = "https://sarkari-genius.onrender.com";
+// const BASE_URL = "http://localhost:7000";
 
 
 const APIGovtJobs = {
