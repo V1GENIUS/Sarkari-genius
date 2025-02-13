fe5ad63140082dc24312bc6033be44d94da0dee3
V1GENIUS <Vivekrathore9111@gmail.com>
2025-02-13 16:06:53 +0530

    second commitss

--- src/Pages/GovtJobDetails.jsx
+++ src/Pages/GovtJobDetails.jsx
@@ -56,7 +56,7 @@ function GovtJobDetails() {
     if (!jobDetails) return "";
     const jobLink = `${window.location.origin}/job-detail/${id}`; 
     return `
-     ${jobDetails.vacancy } भर्ती एव आवेदन!
+     ${jobDetails.postName } भर्ती एव आवेदन!
   *Organization* : ${jobDetails.organization }
   *Post Name* : ${jobDetails.postName }
   *Vacancies* : ${jobDetails.vacancy }
